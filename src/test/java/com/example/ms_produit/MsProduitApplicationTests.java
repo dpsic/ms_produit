6fdac29081c4c1fb1143e8be43c242775587929d
softApp-eng <softmed2018@gmail.com>
2021-11-26 09:07:15 +0100

    Initial commit

---
+++ src/test/java/com/example/ms_produit/MsProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_produit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProduitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
